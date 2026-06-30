19346d9680700d390866265dfc7dfcf2c0775fa1
galibremo <galibremo@gmail.com>
2026-06-30 15:18:00 +0600

    updated

--- src/app/[locale]/(dashboard)/layout.tsx
+++ src/app/[locale]/(dashboard)/layout.tsx
@@ -1,35 +1,33 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
 import { SiteHeader } from "@/components/layout/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { TeamDeselectProvider } from "@/components/layout/team-deselect-provider";
 
 import { BreadcrumbProvider } from "@/providers/breadcrumb-provider";
 
 export default function DashboardLayout({ children }: Readonly<GlobalLayoutProps>) {
 	return (
 		<BreadcrumbProvider>
-			<TeamDeselectProvider>
-				<SidebarProvider
-					style={
-						{
-							"--sidebar-width": "calc(var(--spacing) * 72)",
-							"--header-height": "calc(var(--spacing) * 12)"
-						} as React.CSSProperties
-					}
-				>
-					<AppSidebar variant="inset" />
-					<SidebarInset>
-						<SiteHeader />
-						<div className="flex flex-1 flex-col">
-							<div className="@container/main flex flex-1 flex-col gap-2">
-								<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-									<div className="px-4 lg:px-6">{children}</div>
-								</div>
+			<SidebarProvider
+				style={
+					{
+						"--sidebar-width": "calc(var(--spacing) * 72)",
+						"--header-height": "calc(var(--spacing) * 12)"
+					} as React.CSSProperties
+				}
+			>
+				<AppSidebar variant="inset" />
+				<SidebarInset>
+					<SiteHeader />
+					<div className="flex flex-1 flex-col">
+						<div className="@container/main flex flex-1 flex-col gap-2">
+							<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+								<div className="px-4 lg:px-6">{children}</div>
 							</div>
 						</div>
-					</SidebarInset>
-				</SidebarProvider>
-			</TeamDeselectProvider>
+					</div>
+				</SidebarInset>
+			</SidebarProvider>
 		</BreadcrumbProvider>
 	);
 }
+
